9ea1465d05611b9958e4bf8d400eefa390d27487
David J. H. Shih <djh.shih@gmail.com>
2019-10-17 16:41:05 -0500

    Added vcf2snv.

--- bin/vcf2snv.sh
+++ bin/vcf2snv.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
+# Extract SNV information from VCF file.
 
 set -euo pipefail
 
-if [[ $# -ge 3 || $# -ge 1 && $1 == "-h" ]]; then
-	echo "usage $0 [in.vcf | in.vcf.gz] [out.snv]" >&2
+if (( $# < 1 )) ; then
+	echo "usage: $0 <vcf | vcf.gz>"
 	exit 1
 fi
 
-if [[ $# -ge 1 ]]; then
-	infile=$1
-else
-	infile=/dev/stdin
-fi
-
-if [[ $# -ge 2 ]]; then
-	outfile=$2
-else
-	outfile=/dev/stdout
-fi
+infile=$1
 
 fname=${infile##*/}
-fext=${fname##*.}
-fstem=${fname%%.*}
+fstem=${fname%.*}
+outfile=${fstem}.snv
 
-outfile=${outfile:-${fstem}.snv}
-
-if [[ $fext == "gz" ]]; then
-	cat=zcat
+if [[ ${infile##*.} == "gz" ]]; then
+	reader="gunzip -c"
 else
-	cat=cat
+	reader="cat"
 fi
 
-printf 'chrom\tpos\tref\talt\n' > $outfile
-$cat $infile | grep -v '^#' | cut -f 1,2,4,5 >> $outfile
-
+printf "chrom\tpos\tref\talt\n" > $outfile
+$reader $infile | grep -v '^#' | cut -f 1,2,4,5 >> $outfile
